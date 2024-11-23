b05050955b5f7d3c6b1e1f1cac769bffe993f616
andrewrade <andrewrdeur@gmail.com>
2024-11-23 16:34:17 -0500

    test transforms

--- encoder_train.py
+++ encoder_train.py
@@ -45,7 +45,7 @@ def augment_data(imgs):
     transforms = v2.Compose([
         v2.RandomHorizontalFlip(0.5),
         v2.RandomVerticalFlip(0.5),
-        v2.RandomAffine(degrees=(0, 30), scale=(1, 0.9)),
+        v2.RandomAffine(degrees=(0, 30), scale=(0.9, 1)),
         v2.GaussianBlur(kernel_size=3,sigma=(0.1, 1)),
     ])
     
